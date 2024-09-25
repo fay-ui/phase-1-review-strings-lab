eca5752f6b9f6df36c6a70dd49b83ac05fe09b43
fay-ui <faith.njau@student.moringaschool.com>
2024-09-25 17:23:42 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Faith Njau';
+ const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+ 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()+`${currentUser}`.toUpperCase()+'!'+currentUser.toLowerCase+'!' ;
+                                                                                      
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
